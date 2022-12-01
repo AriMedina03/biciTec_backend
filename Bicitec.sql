a640c258578c80e547d3e14525b6ac978effca78
Your Name <you@example.com>
2022-12-01 04:49:27 -0600

    API IOT

--- Bicitec.sql
+++ Bicitec.sql
@@ -1,25 +1,37 @@
-
-drop table bicycle; 
-drop table rackControl; 
-drop table rackControl; 
-drop table slotControl; 
-drop table bicycleInfoLogs;
-
+DROP DATABASE IF EXISTS IoT;
+-- Para crear la base de datos
+CREATE DATABASE IoT;
+-- Para conectarme a la base de datos
+use IoT;
 CREATE TABLE bicycleControl (
-        bicycleId varchar(11) NOT NULL,
+        bicycleId varchar(20) NOT NULL,
         PRIMARY KEY (bicycleId),
         position_x int, 
         position_y int, 
         vel float, 
         accel float, 
-        ts TIMESTAMP
+        ts DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+insert into bicycleControl (bicycleId, position_x, position_y, vel, accel) 
+values ( 'ASW', 3, 4, 3, 3);
+
+SELECT * FROM bicycleControl;
+
+
+INSERT INTO bicycleControl (bicycleId, position_x, position_y, vel, acces, ts) VALUES ('as3', 45, 55, 66, 78, timestamp());
+
+drop table bicycleControl;
+drop table rackControl;
+
 CREATE TABLE rackControl (
         rackId INT NOT NULL AUTO_INCREMENT,
-        x INT, -- x coordinate
-        y INT, -- y coordinate
+        rackX INT, -- x coordinate
+        rackY INT, -- y coordinate
         slotID varchar(20),
         bicycle varchar (11),
-        PRIMARY KEY (rackId)
-        FOREIGN KEY (bicycle) REFERENCES bicycle(bicycleId)
+        PRIMARY KEY (rackId),
+        FOREIGN KEY (bicycle) REFERENCES bicycleControl(bicycleId)
 );
+
+SELECT * FROM iot.rackControl
