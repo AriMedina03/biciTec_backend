8cf92ede68725027f62ef076803ff947bc8394f0
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2022-11-30 19:56:50 -0600

    este es mi cambio

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const express  = require ('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {res.send ('hola')})
-app.listen(3001, ()=> {console.log ('callback')})
+app.get("/", (req, res) => {
+  res.send("hola");
+});
+app.listen(3001, () => {
+  console.log("cambio de adrian");
+});
